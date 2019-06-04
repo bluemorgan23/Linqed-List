9851298eb96b9061e52a489d939c821615fca5c7
Chris Morgan <bluemorgan2323@gmail.com>
2019-06-04 10:57:54 -0500

    Sqr root TakeWhile

--- Program.cs
+++ Program.cs
@@ -75,6 +75,34 @@ namespace Linqed_list
             double sum = purchases.Sum();
 
             Console.WriteLine(sum);
+            
+            Console.WriteLine("- - - - - - - - - -");
+
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            bool IsPerfectSquare(int num)
+            {
+                int closestRoot = (int) Math.Sqrt(num);
+                return num == closestRoot * closestRoot;
+            }
+
+            IEnumerable<int> sqr = wheresSquaredo
+                .TakeWhile(n => !IsPerfectSquare(n));
+
+            foreach(int num in sqr)
+            {
+                Console.WriteLine(num);
+            }
+
         }
     }
 }
