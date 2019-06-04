13fc5555773d7156afce7446a19e86f0d0e2dc96
Chris Morgan <bluemorgan2323@gmail.com>
2019-06-04 09:58:04 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linqed_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
