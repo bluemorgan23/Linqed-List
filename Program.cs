e2f9b2c5c227586e148032dc09ca2fef2a94406f
Chris Morgan <bluemorgan2323@gmail.com>
2019-06-04 10:19:03 -0500

    Names in descending order

--- Program.cs
+++ Program.cs
@@ -12,12 +12,31 @@ namespace Linqed_list
             List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
             IEnumerable<string> LFruits = from fruit in fruits
-                where fruit.StartsWith("L")
-                select fruit;
+                                          where fruit.StartsWith("L")
+                                          select fruit;
 
-            foreach(string fruit in LFruits)
+            foreach (string fruit in LFruits)
             {
                 Console.WriteLine(fruit);
+            }
+
+            Console.WriteLine("- - - - - - - - - -");
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending(name => name.First()).ToList();
+
+            foreach(string name in descend)
+            {
+                Console.WriteLine(name);
             }
         }
     }
