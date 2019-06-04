f88935c7900e5415a31435397078f77908519746
Chris Morgan <bluemorgan2323@gmail.com>
2019-06-04 10:23:50 -0500

    Aggregate operations

--- Program.cs
+++ Program.cs
@@ -34,10 +34,47 @@ namespace Linqed_list
 
             List<string> descend = names.OrderByDescending(name => name.First()).ToList();
 
-            foreach(string name in descend)
+            foreach (string name in descend)
             {
                 Console.WriteLine(name);
             }
+
+            Console.WriteLine("- - - - - - - - - -");
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            List<int> descendNum = numbers.OrderBy(num => num).ToList();
+
+            foreach(int num in descendNum)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("- - - - - - - - - -");
+
+            // Output how many numbers are in this list
+            List<int> numbersList = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            Console.WriteLine(numbersList.Count);
+
+            Console.WriteLine("- - - - - - - - - -");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            double sum = purchases.Sum();
+
+            Console.WriteLine(sum);
         }
     }
 }
